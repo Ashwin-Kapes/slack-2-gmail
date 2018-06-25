47f223cf9adcbf95f652a672aa88a96a059f41bf
Samuel Berthe <dev@samuel-berthe.fr>
2018-06-25 20:37:29 +0200

    https things

--- app/app/Http/Middleware/Custom/ForceHttps.php
+++ app/app/Http/Middleware/Custom/ForceHttps.php
@@ -16,8 +16,7 @@ class ForceHttps
     public function handle($request, Closure $next)
     {
         if (!$request->secure() && in_array(env('APP_ENV'), ['stage', 'staging', 'preprod', 'preproduction', 'prod', 'production'])) {
-            // disabled for now because reverse proxy does not forward https :-(
-            // return redirect()->secure($request->getRequestUri());
+            return redirect()->secure($request->getRequestUri());
         }
 
         return $next($request);
